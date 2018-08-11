3de8981d795f844d1369246404e87c31cefe3e30
Yuto Nakamura <yutonakamura0320@gmail.com>
2018-08-11 22:01:35 +0900

    2018/08/11

---
+++ Todo/AddController.swift
@@ -0,0 +1,24 @@
+//
+//  AddController.swift
+//  Todo
+//
+//  Created by Yuto Nakamura on 2018/08/11.
+//  Copyright © 2018年 Yuto Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class AddController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
